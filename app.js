747d613135cec1789d9a6ac59d4b3039bdd45cb2
Tin Le <tin.le@adurolife.com>
2019-05-23 17:15:07 +0700

    add json and urlencoded support

--- app.js
+++ app.js
@@ -5,14 +5,16 @@ var port = process.env.PORT || 3000;
 
 /* Express */
 const app = express();
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get("/getData", (req, res) => {
-    var url = req.body.url || 'https://www.gosugamers.net/dota2';
-    fetch(url).then(function(res) {
-        return res.text();
-    }).then(function(html) {
-        return res.json({ success: true, data: html });
-    });
+  var url = req.body.url;
+  fetch(url).then(function(res) {
+      return res.text();
+  }).then(function(html) {
+      return res.json({ success: true, data: html });
+  });
 });
 
 app.get("/", (req, res) => {
